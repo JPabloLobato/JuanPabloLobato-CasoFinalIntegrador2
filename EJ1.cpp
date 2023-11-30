6eb51f657595e6f7e171cfe93db3941a2bc7e9ac
JPabloLobato <jlobagon@myuax.com>
2023-11-30 12:05:04 +0100

    Ej1 casi completado

--- EJ1.cpp
+++ EJ1.cpp
@@ -8,6 +8,19 @@ void load_script(const char* filename, bool show_script = false) {
         std::cerr << "No se pudo abrir el archivo " << filename << std::endl;
         return 0;
     }
-
+    std::string line;
+    while (std::getline(file, line)) {
+        if (show_script) {
+            std::cout << line << std::endl;
+        }
+    }
+    file.close();
+}
+void load_Script () {
+    std::string filename;
+    std::cout << "Ingrese el nombre del archivo: ";
+    std::cin >> filename;
+    load_script(filename.c_str();
+}
 
 
