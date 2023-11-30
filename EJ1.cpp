85c1d4b2fa28a7e197bc4226f32703fe956f1cf9
JPabloLobato <jlobagon@myuax.com>
2023-11-30 12:01:27 +0100

    apartado 1

--- EJ1.cpp
+++ EJ1.cpp
@@ -2,3 +2,12 @@
 #include <string>
 #include <fstream>
 
+void load_script(const char* filename, bool show_script = false) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "No se pudo abrir el archivo " << filename << std::endl;
+        return 0;
+    }
+    std
+
+
